fb1bc052b47a5583d40d365c088d5b90c1d5a8fb
lelopez <ux@lelopez.io>
2020-06-18 00:56:21 -0500

    formatted purchase page

--- src/pages/payments.js
+++ src/pages/payments.js
@@ -28,11 +28,42 @@ const PaymentsPage = (data) => {
         <Layout>
             <div className="container">
                 <article className="content" style={{ textAlign: `center` }}>
-                    <h1 className="content-title">subscript</h1>
                     <section className="content-body">
-                        <button role="link" onClick={handleClick}>
-                            Subscribe
-                        </button>
+                        <h1>Services for growing businessess.</h1>
+                        <h2 className="custom-page-h2">
+                            Trusted infrastructure extended to meet your needs.
+                        </h2>
+                        <div className="purchase-wrapper">
+                            <div className="purcase-card card-box-shadow">
+                                <div className="purcase-card-content">
+                                    <h1>Simple Web Hosting</h1>
+                                    <p>
+                                        A recurring payment for simple web
+                                        hosting of a site.
+                                    </p>
+                                    <img src="images/cloud.png" />
+                                    <p className="pricing">
+                                        $14.99 <span>+ tax</span>
+                                    </p>
+                                    <p>per month</p>
+                                </div>
+
+                                <button role="link" onClick={handleClick}>
+                                    Subscribe
+                                </button>
+                            </div>
+                            <div className="purcase-card card-dark">
+                                <div className="purcase-card-content">
+                                    <h1>Custom</h1>
+                                    <p>
+                                        Feel free to contact us if you project
+                                        requires custom work.
+                                    </p>
+                                </div>
+
+                                <button role="link">Email</button>
+                            </div>
+                        </div>
                     </section>
                 </article>
             </div>
