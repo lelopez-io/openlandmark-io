0203debffafbb8fe8d8e7891f08d0ec029cb4e4c
lelopez <ux@lelopez.io>
2020-06-17 21:14:31 -0500

    updated footer text

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -151,7 +151,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                     <footer className="site-foot">
                         <div className="site-foot-nav container">
                             <div className="site-foot-nav-left">
-                                <Link to="/">{site.title}</Link> © 2019
+                                <Link to="/">{site.title} LLC</Link> © 2020
                             </div>
                             <div className="site-foot-nav-right">
                                 {/* <Navigation
