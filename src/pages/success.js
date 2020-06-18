ab811730224460253158a28a3454ab26f23d0625
lelopez <ux@lelopez.io>
2020-06-18 01:53:25 -0500

    added a cancel and success page

---
+++ src/pages/success.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { Layout } from "../components/common";
+import { Link } from "gatsby";
+
+const PaymentsPage = (data) => (
+    <Layout>
+        <div className="container">
+            <article className="content" style={{ textAlign: `center` }}>
+                <section className="content-body">
+                    <h1>Your purchase went through successfully</h1>
+                    <h2 className="custom-page-h2">
+                        Return <Link to="/">home</Link>.
+                    </h2>
+                </section>
+            </article>
+        </div>
+    </Layout>
+);
+
+export default PaymentsPage;
