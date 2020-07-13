271e727bfa31ecb688e1f89eb66e102951aefff2
lelopez <ux@lelopez.io>
2020-07-13 00:34:00 -0500

    updated layout to use common components

--- src/components/common/ghost/layout.tsx
+++ src/components/common/ghost/layout.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
-import { Link, StaticQuery, graphql } from "gatsby";
-import Img from "gatsby-image";
+import { StaticQuery, graphql } from "gatsby";
 
-import { Navigation } from ".";
 import config from "../../../utils/site-config";
+
+import { Section, Container } from "../../global";
+
+import Navigation from "../navigation/navigation";
+import Header from "../../sections/header";
+import Footer from "../../sections/footer";
+
+import styled, { ThemeProvider } from "styled-components";
+
+import theme from "../../../styles/theme";
+import GlobalStyles from "../../../styles/global-styles";
 
 // Styles - for Ghost pages
 import "../../../assets/css/app.css";
@@ -20,96 +29,25 @@ import "../../../assets/css/app.css";
  */
 const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
     const site = data.allGhostSettings.edges[0].node;
-    const twitterUrl = site.twitter
-        ? `https://twitter.com/${site.twitter.replace(/^@/, ``)}`
-        : null;
-    const facebookUrl = site.facebook
-        ? `https://www.facebook.com/${site.facebook.replace(/^\//, ``)}`
-        : null;
 
     return (
-        <>
-            <Helmet>
-                <html lang={site.lang} />
-                <style type="text/css">{`${site.codeinjection_styles}`}</style>
-                <body className={bodyClass} />
-            </Helmet>
+        <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <>
+                <Helmet>
+                    <html lang={site.lang} />
+                    <style type="text/css">{`${site.codeinjection_styles}`}</style>
+                    <body className={bodyClass} />
+                </Helmet>
+                <Navigation />
+                {isHome ? <Header /> : null}
 
-            <div className="viewport">
-                <div className="viewport-top">
-                    {/* The main header section on top of the screen */}
-                    <header
-                        className="site-head"
-                        style={{
-                            ...(site.cover_image && {
-                                backgroundImage: `url(${site.cover_image})`,
-                            }),
-                        }}
-                    >
-                        <div className="container">
-                            <div className="site-mast">
-                                <div className="site-mast-left">
-                                    <Link className="site-nav-item" to="/">
-                                        home
-                                    </Link>
-                                </div>
-                                <div className="site-mast-right">
-                                    <Link
-                                        className="site-nav-button"
-                                        to="/shop"
-                                    >
-                                        shop
-                                    </Link>
-                                </div>
-                            </div>
-                            {isHome ? (
-                                <div className="site-banner">
-                                    {site.logo ? (
-                                        <img
-                                            className="site-banner-logo"
-                                            src={site.logo}
-                                            alt={site.title}
-                                        />
-                                    ) : (
-                                        <Img
-                                            fixed={
-                                                data.file.childImageSharp.fixed
-                                            }
-                                            alt={site.title}
-                                        />
-                                    )}
-                                    <h1 className="site-banner-title">
-                                        {site.title}
-                                    </h1>
-                                </div>
-                            ) : null}
-                        </div>
-                    </header>
-
-                    <main className="site-main">
-                        {/* All the main content gets inserted here, index.js, post.js */}
-                        {children}
-                    </main>
-                </div>
-
-                <div className="viewport-bottom">
-                    {/* The footer at the very bottom of the screen */}
-                    <footer className="site-foot">
-                        <div className="site-foot-nav container">
-                            <div className="site-foot-nav-left">
-                                <Link to="/">{site.title} LLC</Link> © 2020
-                            </div>
-                            <div className="site-foot-nav-right">
-                                {/* <Navigation
-                                    data={site.navigation}
-                                    navClass="site-foot-nav-item"
-                                /> */}
-                            </div>
-                        </div>
-                    </footer>
-                </div>
-            </div>
-        </>
+                <StyledSection>
+                    <Container>{children}</Container>
+                </StyledSection>
+                <Footer />
+            </>
+        </ThemeProvider>
     );
 };
 
@@ -148,3 +86,7 @@ const DefaultLayoutSettingsQuery = (props) => (
 );
 
 export default DefaultLayoutSettingsQuery;
+
+const StyledSection = styled(Section)`
+    padding-top: 160px;
+`;
