867bcc9ed2a621d2c478ba8adc41fb500e378df4
Andres Rojas <andres@openlandmark.io>
2021-03-10 20:45:28 -0600

    updated site information

--- src/utils/site-config.js
+++ src/utils/site-config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    url: `https://sphere.club`, // Site domain. Do not include a trailing slash!
-    title: `Join the Club`, // Used for App manifest e.g. Mobile Home Screen
+    url: `https://openlandmark.io`, // Site domain. Do not include a trailing slash!
+    title: `Openlandmark`, // Used for App manifest e.g. Mobile Home Screen
     icon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
 
     metadata: {
-        title: `Sphere`, // This allows an alternative site title for meta data for pages.
-        description: `Sign up for powa and get access to our Club community featuring outdoor workouts in a city near you`, // This allows an alternative site description for meta data for pages.
-        author: `@`,
+        title: `Openlandmark`, // This allows an alternative site title for meta data for pages.
+        description: `Become Global: Build with Trusted Infrastructure `, // This allows an alternative site description for meta data for pages.
+        author: `@openlandmark`,
     },
 
     nav: {
